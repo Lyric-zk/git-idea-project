e4ed0dc54a79b1a2d5b33cfcf9893ac2e9ea75af
Lyric-zk <138542109+Lyric-zk@users.noreply.github.com>
2023-07-04 16:21:29 +0800

    Update helloGithub.java
    second commit

--- src/test/helloGithub.java
+++ src/test/helloGithub.java
@@ -3,5 +3,6 @@ package test;
 public class helloGithub {
     public static void main(String[] args) {
         System.out.println("Hello Github!!!!");
+        System.out.println("lallalalalala！！");
     }
 }
