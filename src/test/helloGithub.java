cdfb42e092bd75bff73bb92fec2f79f68a841bf2
Lyric-zk <138542109+Lyric-zk@users.noreply.github.com>
2023-07-05 10:49:32 +0800

    Github update

--- src/test/helloGithub.java
+++ src/test/helloGithub.java
@@ -4,5 +4,6 @@ public class helloGithub {
     public static void main(String[] args) {
         System.out.println("Hello Github!!!!");
         System.out.println("lallalalalala！！");
+        System.out.print("Github update!!!!1");
     }
 }
